adcbf61e3ce7193c3878540700e6bcb2fafd8ec7
Hegeldirkk <hegeldirkk@gmail.com>
2022-12-20 11:07:31 +0000

    ok valid ok

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: 'standard',
+  extends: "eslint:recommended",
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
   rules: {
     "semi": ["error", "always"],
     "quotes": ["error", "double"]
   }
-}
+};
